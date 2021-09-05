bdcea63e46035e73317cd068a79b6baab0ccf576
Assassins2000 <css.ctt5@gmail.com>
2021-09-06 01:57:32 +0800

    User domain model has been added

--- src/domains/models/user.model.ts
+++ src/domains/models/user.model.ts
@@ -1,11 +1,11 @@
 export class DUser {
-  constructor(private id: string, private name: string, private login: string, private password: string) {}
+  constructor(private id: string, private name: string, private login: string, private password?: string) {}
 
   public get getId(): string {
     return this.id;
   }
 
-  public get getPassword(): string {
-    return this.password;
+  public get getPassword(): string | null {
+    return this.password ?? null;
   }
 }
