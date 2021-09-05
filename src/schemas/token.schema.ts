764e61f25d453224281e1aaf2764b2338dd7cefd
Assassins2000 <css.ctt5@gmail.com>
2021-09-06 02:04:35 +0800

    Refactoring Mongoose schemas

--- src/schemas/token.schema.ts
+++ src/schemas/token.schema.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { User } from '../user/user.schema';
+import { User, UserDocument } from './user.schema';
 import { Document } from 'mongoose';
 
 export type TokenDocument = Token & Document;
@@ -8,7 +8,7 @@ export type TokenDocument = Token & Document;
 @Schema()
 export class Token {
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: User.name })
-  user!: User;
+  user!: UserDocument;
 
   @Prop({ required: true })
   key!: string;
