55543763d305c50b6f04a0c97c9b1d018b69bbc3
Assassins2000 <css.ctt5@gmail.com>
2022-07-26 00:42:09 +0800

    Fix import

--- src/modules/simulatorSection/createAndCloseSession/types/userHasActiveSessionError.type.ts
+++ src/modules/simulatorSection/createAndCloseSession/types/userHasActiveSessionError.type.ts
@@ -1,3 +1,4 @@
 import { SessionServiceErrorCode } from '../constants/sessionServiceErrorCode.enum';
+import { BaseServiceError } from '@app/sharedModules/base/baseServiceError.interface';
 
 export type UserHasActiveSessionsError = BaseServiceError<SessionServiceErrorCode>;
